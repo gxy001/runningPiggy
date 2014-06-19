5d2c51f73c2a1d14df54f5da6c693602c132f3ec
gxy001 <xiaoying@xiaobai.local>
2014-06-19 00:45:51 -0700

    Add Google map and show the current position at the New Workout tab

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,51 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
+        "id": "org.apache.cordova.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
+        "id": "org.apache.cordova.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.console": "0.2.9",
-    "org.apache.cordova.device": "0.2.10"
+    "org.apache.cordova.device": "0.2.10",
+    "org.apache.cordova.device-motion": "0.2.8",
+    "org.apache.cordova.device-orientation": "0.3.7",
+    "org.apache.cordova.geolocation": "0.3.8"
 }
 // BOTTOM OF METADATA
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('NewWorkoutCtrl', function($scope, $rootScope, $timeout) {
+//Set timer
             $scope.hasStarted = false;
             $scope.buttonCaption = "Start Workout";
             $scope.timerString = "00:00:00";
@@ -33,7 +34,7 @@ angular.module('starter.controllers', [])
                 var diffTime = currentTime - startTime;
                 getTime(diffTime/1000);
                 mytimeout = $timeout($scope.onTimeout,1000);
-            }
+            };
             
             var getTime = function(t){
                 t = Math.floor(t);
@@ -41,7 +42,7 @@ angular.module('starter.controllers', [])
                 var MM = Math.floor(t / 60) - hh * 60;
                 var ss = t - hh * 60 * 60 - MM * 60;
                 setTimeString(hh, MM, ss);
-            }
+            };
             
             var setTimeString = function(hh, MM, ss){
                 var hhString = hh;
@@ -57,7 +58,45 @@ angular.module('starter.controllers', [])
                     ssString = "0" + ss;
                 }
                 $scope.timerString = hhString + ":" + MMString + ":" + ssString;
-            }
+            };
+
+            
+//Google map operation
+            var latitude = 31;
+            var longitude = 121;
+            $scope.model = { runningMap: undefined };
+            $scope.mapMarkers = [];
+            $scope.mapOptions = {
+                center: new google.maps.LatLng(latitude, longitude),
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            
+            var getGeolocation = function(){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                }
+                else {
+                    alert("Geolocation is not supported.");
+                }
+            };
+            
+            var onSuccess = function(position){
+                longitude = position.coords.longitude;
+                latitude = position.coords.latitude;
+                var latlng = new google.maps.LatLng(latitude, longitude);
+                $scope.model.runningMap.setCenter(latlng);
+                $scope.mapMarkers.push(new google.maps.Marker({ map: $scope.model.runningMap, position: latlng }));
+
+                $scope.$apply();
+            };
+            
+            var onError = function(error){
+                alert('code: ' + error.code + '\n' + 'message:' + error.message + '\n');
+                $scope.$apply();
+            };
+            
+            getGeolocation();
 })
 
 .controller('WorkoutDetailCtrl', function($scope, $stateParams, Friends) {
